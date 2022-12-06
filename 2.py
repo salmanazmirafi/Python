c77b95fdfd8fd25eb4d6c123db90f9d5c693ce8b
Salman Azmi Rafi <salmanazmirafipersonal@gmail.com>
2022-12-06 19:18:47 +0600

    last update 12/6/2022

---
+++ 2.py
@@ -0,0 +1,59 @@
+#---------------Chapter 02 variable and data type----------------------
+
+# Fast variable 
+fruits = "Mango"
+# Variable Value Change
+fruits = "Orange"
+# Out Put
+print(fruits)
+
+# Comment
+# 2nd Comment
+# 3rd Comment
+# 4th Comment
+
+
+#---------------------------------------------------
+
+# Rules For Naming Variable
+
+# Right Way#
+#fruits ="Mango"
+#my_fv_fruits = "Mango"
+#fName = "Mango"
+#userName = "Mango"
+# Wrong Way
+#$fruits = "Wrong"
+#my fruits = "Wrong"
+#my-fruits = "Wrong"
+
+
+
+
+
+#---------------------------------------------------
+
+# Data type in python
+
+# Number
+# String
+# Booleans
+# List
+# Tupple
+# Set
+# Dictionary
+
+# number
+number = 4
+integer = 21
+floating = 1.39
+
+# string
+string = "name"
+
+# booleans
+x = True
+y = False
+
+
+#---------------------------------------------------
---
+++ 3.py
@@ -0,0 +1,41 @@
+#-----------------------Chapter 03 Operator in python----------------------------
+
+
+#--------------------Type of operator-------------------------------
+
+#--------------------Arithmetic Operator-------------------------------
+
+# Addition
+x = 5+10
+print(x)
+
+# Subtraction
+x = 15-10
+print(x)
+
+# Multipitication
+x = 5/10
+print(x)
+
+# Division
+x = 5%10
+print(x)
+
+# Exponenttation
+x = 5**10
+print(x)
+
+# Reminder
+x = 5*10
+print(x)
+
+# Flor devision
+x = 5//10
+print(x)
+
+
+#--------------------Assignment Operator
+#--------------------Comparison Operator
+#--------------------Logical Operator
+#--------------------Identty Operator
+#--------------------Membership Operator
