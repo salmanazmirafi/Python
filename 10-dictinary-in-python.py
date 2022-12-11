8ba9de9fe2a352c5e9f275e1b1c05f8ddfe83c1a
salmanazmirafi <116106498+salmanazmirafi@users.noreply.github.com>
2022-12-11 18:47:56 +0600

    Update

---
+++ 10-dictinary-in-python.py
@@ -0,0 +1,14 @@
+# # # --------------Chapter -09 Dictionary in python------------------
+
+3 hors 29 minites 39 secend
+# creating Dictionary
+
+
+# accessing item
+
+
+# changing new items
+
+
+# changing on item's value
+
